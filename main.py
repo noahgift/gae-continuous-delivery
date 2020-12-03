fd0f1eefc057655d044650d5c6365d03d4586fba
Noah Gift <noah.gift@gmail.com>
2020-12-03 11:47:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! CD.  This is auto-deploy'
+    return 'Hello World! CD.  This is auto-deploy.  Take 10'
 
 @app.route('/echo/<name>')
 def echo(name):
